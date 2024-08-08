a1b4caf7d14134103fe94f9b68c47b5f5ce05079
brianngumbau <brianngumbau9075@gmail.com>
2024-08-08 01:05:20 -0700

    min operations func

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Minimum Operations"""
+
+
+def minOperations(n):
+    """Calculates the fewest number of operations needed to
+    result in exactly n H characters in a file"""
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n /= factor
+        factor += 1
+
+    return operations
